c5428da0471baad2b03617a0a35b3e082986f5f7
Indra <indra@coderbuzz.com>
2026-06-21 11:28:13 +0000

    chore: add CI workflow + smoke test

--- test/smoke.test.ts
+++ test/smoke.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "bun:test";
-import { createServer, createAsyncServer } from "@coderbuzz/kvs-server";
+import { createServer, createAsyncServer } from "../src/index";
 
 test("exports exist", () => {
   expect(typeof createServer).toBe("function");
